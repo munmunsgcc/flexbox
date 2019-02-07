2dc0799a604fc5bb91557198006a22989a5eb2f4
Cheng Mun Mun <munmun@sgcodecampus.com>
2019-02-07 09:45:03 +0800

    Added CircleCI example config

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+
 import Container from './components/Container';
 import Grid from './components/Grid';
+import ComponentA from './components/ComponentA';
 
 export default () => (
   <div>
     <Container />
     <Grid />
+    <ComponentA />
   </div>
 );
---
+++ src/components/ComponentA.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const ComponentA = () => <div>Hello from ComponentA</div>;
+
+export default ComponentA;
